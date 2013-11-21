f6e5a8b6dd09ee309ffdb21b1c265b891b704722
liulei <lo2007@126.com>
2013-11-21 05:06:35 -0800

    add some classes about interface

---
+++ src/main/java/com/liulei/thinkinginjava/interface9/inner/Ball.java
@@ -0,0 +1,15 @@
+package com.liulei.thinkinginjava.interface9.inner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Leo
+ * Date: 11/19/13
+ * Time: 10:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Ball {
+
+    void getName();
+
+    void play();
+}
